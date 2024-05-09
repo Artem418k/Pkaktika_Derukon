14ac79bf933317c269d354810c9ba14393aed779
Artem418k <125126012+Artem418k@users.noreply.github.com>
2024-05-09 16:30:05 +0300

    index.php

--- Derukon_KI-20-1_Praktika/Site/index.php
+++ Derukon_KI-20-1_Praktika/Site/index.php
@@ -1,22 +1,24 @@
-<?php $location  = 'index';
-include 'layouts/header.php';
-include 'layouts/navbar.php'; ?>
+<?php 
+    $location = 'index';
+    include 'layouts/header.php';
+    include 'layouts/navbar.php'; 
+?>
 
-    <div class="container mt-4">
-
-        <?php if (!isset($_COOKIE['user'])): ?>
-
+<div class="container mt-4">
+    <?php if (!isset($_COOKIE['user'])): ?>
         <div class="row">
             <div class="col" align="center">
                 <h1>Форма авторизації</h1>
                 <form action="validation-form/auth.php" method="post">
-                <input type="text" class="form-control" name="email" id="email" placeholder="Ввеіть електронну адресу"><br>
-                <input type="password" class="form-control" name="pass" id="pass" placeholder="Введіть пароль"><br>
-                <button class="btn btn-success" type="submit">Авторизуватися</button>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="Ввеіть електронну адресу"><br>
+                    <input type="password" class="form-control" name="pass" id="pass" placeholder="Введіть пароль"><br>
+                    <button class="btn btn-success" type="submit">Авторизуватися</button>
                 </form>
             </div>
-            <?php else: ?>
+        </div>
+    <?php else: ?>
         <meta http-equiv="refresh" content="0;/kabinet.php">
     <?php endif; ?>
-        </div>
+</div>
+
 <?php include 'layouts/footer.php'; ?>
